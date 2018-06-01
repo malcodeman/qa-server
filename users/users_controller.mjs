eba20950a604ebadebda02503c49b60ef28baa19
Amer Karamustafic <26983729+malcodeman@users.noreply.github.com>
2018-06-01 14:09:57 +0200

    Adds find me route for user

--- users/users_controller.mjs
+++ users/users_controller.mjs
@@ -38,3 +38,18 @@ export async function findAll(req, res, next) {
     res.status(400).send(error);
   }
 }
+
+export async function findMe(req, res, next) {
+  try {
+    const id = req.userId;
+    const me = await User.findAll({
+      where: { id },
+      attributes: {
+        exclude: ["password"]
+      }
+    });
+    res.status(200).send(me[0]);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+}
--- users/users_routes.mjs
+++ users/users_routes.mjs
@@ -1,11 +1,13 @@
 import express from "express";
 
-import { findById, findAll } from "./users_controller.mjs";
+import { findById, findAll, findMe } from "./users_controller.mjs";
 import { requireAuthentication } from "../auth/auth_middleware.mjs";
 
 const router = express.Router();
 
 router.use(requireAuthentication);
+
+router.get("/me", findMe);
 router.get("/:id", findById);
 router.get("/", findAll);
 
