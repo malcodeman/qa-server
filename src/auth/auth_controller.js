57d6d697afc4b46d8e117fa8a348698c75f5f756
Amer Karamustafic <26983729+malcodeman@users.noreply.github.com>
2019-04-01 18:46:29 +0200

    Adds better error handling on login

--- src/auth/auth_controller.js
+++ src/auth/auth_controller.js
@@ -20,11 +20,17 @@ export async function login(req, res, next) {
   try {
     const { username, password } = req.body;
     const user = await findUser(null, username);
+
+    if (!user) {
+      res.status(400).send({ exception: "UserNotFoundException" });
+    }
     if (password === user.password) {
       const token = jwt.sign({ id: user.id }, "secret", {
         expiresIn: 86400
       });
       res.status(200).send({ token, user });
+    } else {
+      res.status(401).send({ exception: "NotAuthorizedException" });
     }
   } catch (error) {
     res.status(400).send(error);
